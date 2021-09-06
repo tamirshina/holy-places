d2218fb97499da9d017457a3d1d7716056e0aef5
tamirshina <tamirshina@gmail.com>
2021-09-06 12:42:08 +0300

    Updates

---
+++ precache-manifest.554909549cef1120d4ef15b7e1263919.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95341c68ec53db55d4408afeac98a5f",
+    "url": "/holy-places/index.html"
+  },
+  {
+    "revision": "bf0a2e56027b9051b364",
+    "url": "/holy-places/static/css/main.d2a6e8fb.chunk.css"
+  },
+  {
+    "revision": "476d47c563391158114c",
+    "url": "/holy-places/static/js/2.9a2bef0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/holy-places/static/js/2.9a2bef0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0a2e56027b9051b364",
+    "url": "/holy-places/static/js/main.f8a5d2df.chunk.js"
+  },
+  {
+    "revision": "a3adb367af31197665df",
+    "url": "/holy-places/static/js/runtime-main.a22c96f2.js"
+  },
+  {
+    "revision": "3c5ac1d87a0a96565ea8477267ef4686",
+    "url": "/holy-places/static/media/01-back-ENG.3c5ac1d8.png"
+  },
+  {
+    "revision": "6301226d224559a30571612d91a9079a",
+    "url": "/holy-places/static/media/01-transion-israel.6301226d.mp4"
+  },
+  {
+    "revision": "f621a9d70be81c5441bdd8a745180454",
+    "url": "/holy-places/static/media/02-back-HEB.f621a9d7.png"
+  },
+  {
+    "revision": "21190d101225e4bd701ef829c3f79538",
+    "url": "/holy-places/static/media/02-background.21190d10.png"
+  },
+  {
+    "revision": "9f6996e0b5a91886529f6a04b3985dc2",
+    "url": "/holy-places/static/media/02-new-hand-HEB.9f6996e0.png"
+  },
+  {
+    "revision": "768ec8b47e29d4e5fb1d202d987688ba",
+    "url": "/holy-places/static/media/02-textbox.768ec8b4.png"
+  },
+  {
+    "revision": "b5b63c65027c231559c0b79cbf0edc0a",
+    "url": "/holy-places/static/media/03-background.b5b63c65.png"
+  },
+  {
+    "revision": "5d664e44ac929758c0c3458f702e4863",
+    "url": "/holy-places/static/media/03-jerusalem.5d664e44.png"
+  },
+  {
+    "revision": "7a0b4d3f226ec28d2c648e693d5410da",
+    "url": "/holy-places/static/media/03-map-circle.7a0b4d3f.png"
+  },
+  {
+    "revision": "c0d889f0a61cf97b9ebed8ac5e18aaa2",
+    "url": "/holy-places/static/media/04-first-transition-en.c0d889f0.mp4"
+  },
+  {
+    "revision": "eb7aa83681b69305369265c9bb387836",
+    "url": "/holy-places/static/media/05-first-transion-heb.eb7aa836.mp4"
+  },
+  {
+    "revision": "83ad254d5568ab35daec0ff17216ce2b",
+    "url": "/holy-places/static/media/06-Arrow-ENG.83ad254d.png"
+  },
+  {
+    "revision": "0e3483d770747e4831874308f9d666cf",
+    "url": "/holy-places/static/media/Alexdner-textbox-copy.0e3483d7.png"
+  },
+  {
+    "revision": "07138d5e1e7d7c00e024bc16a6f53f21",
+    "url": "/holy-places/static/media/Aliyah-Church-copy.07138d5e.png"
+  },
+  {
+    "revision": "463f3d41552e7e826b67a4cdbfb719a1",
+    "url": "/holy-places/static/media/PIN.463f3d41.png"
+  },
+  {
+    "revision": "e7a0b0c92215c9ac568d39af04246889",
+    "url": "/holy-places/static/media/Russian-Compound_background2.e7a0b0c9.png"
+  },
+  {
+    "revision": "c3b6f859aaa150d272bcb091fdccd40b",
+    "url": "/holy-places/static/media/TextBox_ascension.c3b6f859.png"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/holy-places/static/media/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "12cc08d585eecf19181f3451d55d6fcc",
+    "url": "/holy-places/static/media/click-point.12cc08d5.png"
+  },
+  {
+    "revision": "556668083e0ee939b068441bc8d2987e",
+    "url": "/holy-places/static/media/elisTextbox.55666808.png"
+  },
+  {
+    "revision": "e50dd6963fd97366649ae1eaa823a297",
+    "url": "/holy-places/static/media/frame-down.e50dd696.png"
+  },
+  {
+    "revision": "90a805a3365ae111cb3808030d6cb7ab",
+    "url": "/holy-places/static/media/mariaTextBox.90a805a3.png"
+  },
+  {
+    "revision": "643a39eb0853d1fc61f21141e381dc6f",
+    "url": "/holy-places/static/media/nicoTextbox.643a39eb.png"
+  },
+  {
+    "revision": "b3510502f75dd2564ddf7d35a086a426",
+    "url": "/holy-places/static/media/religiousMissionTextBox.b3510502.png"
+  },
+  {
+    "revision": "5ac074d1ede1d45fa51fcb6064a35736",
+    "url": "/holy-places/static/media/russianTextbox.5ac074d1.png"
+  },
+  {
+    "revision": "99928b26001c6786c057f656ef09b4e1",
+    "url": "/holy-places/static/media/scroll-icon.99928b26.png"
+  },
+  {
+    "revision": "f34a6a7d36438c60a0e81e8a93c721fb",
+    "url": "/holy-places/static/media/textbox.f34a6a7d.png"
+  },
+  {
+    "revision": "ae0f260f0c0fbd159338fa6330815b76",
+    "url": "/holy-places/static/media/transition-russian-compound.ae0f260f.mp4"
+  },
+  {
+    "revision": "d25c4931093d14b9f6804dc85a63607e",
+    "url": "/holy-places/static/media/trinityTextbox.d25c4931.png"
+  }
+]);
